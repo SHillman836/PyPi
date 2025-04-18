29fc3a9d40edc3026c3bf431e5b75c7b9deb4b84
Sam Hillman <sam.hillman399@gmail.com>
2025-04-18 10:16:49 +0100

    building out pypi scripts

---
+++ scripts/update_version.py
@@ -0,0 +1,39 @@
+import json
+import sys
+import argparse
+from pathlib import Path
+from setuptools import setup, find_packages
+
+
+config_json_path = Path(__file__).parent.joinpath("config.json")
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("new_version", type=str)
+    args = parser.parse_args()
+    return args.new_version
+
+
+def load_config():
+    try: 
+        with open(config_json_path, "r") as json_data:
+            config = json.load(json_data)
+            return config
+    except Exception as e:
+        print(f"Error loading file from '{config_json_path}': {e}", file=sys.stderr)
+        sys.exit(1)
+
+def write_config(new_config: dict):
+    try:
+        with open(config_json_path, "w") as json_data:
+            json.dump(new_config, json_data, indent=4)
+            json_data.write("\n")
+    except Exception as e:
+        print(f"Error writing file to '{config_json_path}': {e}", file=sys.stderr)
+        sys.exit(1)
+
+if __name__ == "__main__":
+    new_version = parse_args()
+    config = load_config()
+    config["version"] = new_version
+    write_config(config)
--- setup.py
+++ setup.py
@@ -1,15 +1,27 @@
+import json
+import sys
+from pathlib import Path
 from setuptools import setup, find_packages
 
+
+config_json_path = Path(__file__).joinpath("config.json")
+
+def read_version():
+    try:
+        with open(config_json_path, "r") as json_data:
+            config = json.load(json_data)
+            return config["version"]
+    except Exception as e:
+        print(f"Error loading version from '{config_json_path}': {e}", file=sys.stderr)
+        sys.exit(1)
+
 setup(
-    name='pypi_ci_package',
-    version='0.1.1',
-    packages=find_packages(),
-    install_requires=[
-        "twine",
-    ],
+    name="pypi_ci_package",
+    version=read_version(),
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    install_requires=["twine"],
     entry_points={
-        'console_scripts': [
-            'cli=src.cli:main',
-        ],
+        "console_scripts": ["cli=src.cli:main"],
     },
 )
